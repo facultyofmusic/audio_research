02b22824de2cbb1d387e59f025a7215ee44a4099
dgli <facultyofmusic@gmail.com>
2017-05-06 04:19:12 -0400

    Add logging.

--- src/Streamer.java
+++ src/Streamer.java
@@ -4,11 +4,14 @@ import javax.sound.sampled.SourceDataLine;
 import javax.swing.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by dgli on 5/3/17.
  */
 public class Streamer extends SwingWorker<Void, Streamer.Frame> {
+    private static final Logger log = Logger.getLogger(Streamer.class.getName());
     public static final int FRAME_SIZE = 512;
 
     private AudioInputStream inputStream;
@@ -19,21 +22,21 @@ public class Streamer extends SwingWorker<Void, Streamer.Frame> {
 
     @Override
     protected Void doInBackground() throws Exception {
-        System.out.println("Starting new streamer.");
+        log.info("Starting new streamer.");
 
         float samples[] = new float[FRAME_SIZE * inputStream.getFormat().getChannels()];
         long transfer[] = new long[samples.length];
         int bytesPerSample = inputStream.getFormat().getSampleSizeInBits() + 7 >> 3;
         byte[] bytes = new byte[samples.length * bytesPerSample];
 
-        System.out.println("Source format: " + inputStream.getFormat());
-        System.out.println("bytes per sample: " + bytesPerSample);
+        log.info("Source format: " + inputStream.getFormat());
+        log.info("bytes per sample: " + bytesPerSample);
 
         SourceDataLine out = AudioSystem.getSourceDataLine(inputStream.getFormat());
         out.open(inputStream.getFormat(), bytes.length);
         out.start();
 
-        System.out.println("Output line openned: " + out);
+        log.info("Output line openned: " + out);
 
         // pad output with leading zeros
         for(int feed = 0; feed < 6; feed++) {
@@ -47,7 +50,7 @@ public class Streamer extends SwingWorker<Void, Streamer.Frame> {
             readAmount = inputStream.read(bytes);
 
             if (readAmount == -1) {
-                System.out.println("EOF REACHED!!");
+                log.info("EOF REACHED!!");
                 break;
             }
 
@@ -65,7 +68,7 @@ public class Streamer extends SwingWorker<Void, Streamer.Frame> {
 
         }
 
-        System.out.println("Streamer ended.");
+        log.info("Streamer ended.");
 
         return null;
     }
